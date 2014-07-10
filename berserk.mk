3ec2d6f3e7318ab64935614678e4a1d180e743bd
aospl5 <testasaccc@gmail.com>
2014-07-10 15:29:24 +0300

    Update and rename slim.mk to berserk.mk

--- berserk.mk
+++ berserk.mk
@@ -1,5 +1,5 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/slim/config/gsm.mk)
+$(call inherit-product, vendor/berserk/config/gsm.mk)
 
 # Boot animation
 TARGET_BOOTANIMATION_NAME := 480
@@ -8,14 +8,14 @@ TARGET_SCREEN_HEIGHT := 480
 TARGET_SCREEN_WIDTH := 320
 
 # Inherit some common slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/berserk/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/e610/full_e610.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := e610
-PRODUCT_NAME := slim_e610
+PRODUCT_NAME := berserk_e610
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-E610
 PRODUCT_MANUFACTURER := LGE
@@ -26,5 +26,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="lge/m4_open_eu/m4:4.1.2/JZO54K/E61020c-EUR-XX.1367460723:user/release-keys" \
     PRIVATE_BUILD_DESC="m4_open_eu-user 4.1.2 JZO54K E61020c-EUR-XX.1367460723 release-keys"
 
-# Enable Torch
-PRODUCT_PACKAGES += Torch
